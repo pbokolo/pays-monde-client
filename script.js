29f72e2e586b9ab1b9627af63ba6be85b836d2ef
pbokolo <dev.pbokolo@gmail.com>
2023-07-30 20:52:31 +0200

    Rendering the country currency

--- script.js
+++ script.js
@@ -18,17 +18,28 @@ const getData = async (country) => {
 
 const renderCountry = (data) => {
   const [country] = data;
-  const element = `<div class="card">
+  const [cur] = country.currencies
+    ? Object.keys(country.currencies)
+    : ["No official"];
+  const { name, symbol } =
+    cur !== "No official"
+      ? country.currencies[`${cur}`]
+      : { name: "No official", symbol: "No official" };
 
+  const element = `<div class="card">
   <div class="card__flag">
-  <img class="card__flag-picture" src=${country.flags.png} alt=${country.name.common}/>
+  <img class="card__flag-picture" src=${country.flags.png} alt=${
+    country.name.common
+  }/>
   </div>
   <div class="card__details">
     <p class="text text--title">${country.name.common}</p>
     <p class="text text--body">${country.capital[0]}</p>
     <p class="text text--body">${country.region}</p>
-    <p class="text text--body">Currency</p>
-    <p class="text text--body">${country.population}</p>
+    <p class="text text--body">${
+      cur ? `${name} ${symbol ? `(${symbol})` : ""}` : "No official"
+    }</p>
+    <p class="text text--body">${formatCompactNumber(country.population)}</p>
   </div>
 </div>`;
   section.innerHTML = element;
@@ -43,6 +54,20 @@ const clearSection = () => {
   const cta = `<p class="text">Commencez par chercher un pays</p>`;
   section.innerHTML = cta;
 };
+
+function formatCompactNumber(number) {
+  if (number < 1000) {
+    return number;
+  } else if (number >= 1000 && number < 1_000_000) {
+    return (number / 1000).toFixed(1) + "K";
+  } else if (number >= 1_000_000 && number < 1_000_000_000) {
+    return (number / 1_000_000).toFixed(1) + "M";
+  } else if (number >= 1_000_000_000 && number < 1_000_000_000_000) {
+    return (number / 1_000_000_000).toFixed(1) + "B";
+  } else if (number >= 1_000_000_000_000 && number < 1_000_000_000_000_000) {
+    return (number / 1_000_000_000_000).toFixed(1) + "T";
+  }
+}
 
 const init = () => {
   formSearch.addEventListener("submit", (e) => {
